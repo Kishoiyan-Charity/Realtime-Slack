31a52e2eaeee098e976f7c578dec558973fa11aa
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-02-09 15:35:05 +0300

    changes

---
+++ components/sign-in-card.tsx
@@ -0,0 +1,15 @@
+import {Card, CardDescription, CardHeader} from "@/components/ui/card";
+
+export const SignInCard = () => {
+    return(
+        <Card className='w-full h-full p-8'>
+            <CardHeader className='px-0 pt-8'>
+                Login to continue
+            </CardHeader>
+
+            <CardDescription>
+            </CardDescription>
+            Sign In Card
+        </Card>
+    )
+}
--- features/auth/components/auth-screen.tsx
+++ features/auth/components/auth-screen.tsx
@@ -2,12 +2,16 @@
 
 import {useState} from "react";
 import {SignInFlow} from "@/features/auth/types";
+import {SignInCard} from "@/components/sign-in-card";
+import {SignUpCard} from "@/components/sign-up-card";
 
 export const AuthScreen = () => {
-    const [state, setState] = useState<SignInFlow>()
+    const [state, setState] = useState<SignInFlow>("signIn")
     return (
-        <div>
-            Auth Screen
+        <div  className="h-full flex items-center justify-center bg-[#5C3B58]">
+            <div className="md:h-auto md:w-105">
+                {state === "signIn" ? <SignInCard /> : <SignUpCard />}
+            </div>
         </div>
     )
 }
